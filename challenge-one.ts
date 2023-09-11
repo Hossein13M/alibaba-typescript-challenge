fbb4676368b1673175cf71c13c072b81ba5896e6
ahmadreza <ah.tahmasbi@alibaba.ir>
2023-09-11 16:08:19 +0330

    feat: implement first question

--- challenge-one.ts
+++ challenge-one.ts
@@ -1,1 +1,8 @@
-// Implement a generic called First<T> that takes an Array T and returns its first element's type.
+type First<T extends Array<any>> = T[0]
+
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type test = First<arr2>
+
+let a : test = 3
