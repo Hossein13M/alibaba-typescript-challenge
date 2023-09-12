27337ca08f9ee926626b35bb7a1923e3de357817
ahmadreza <ah.tahmasbi@alibaba.ir>
2023-09-12 10:52:12 +0330

    feat: implement second question

--- challenge-two.ts
+++ challenge-two.ts
@@ -1,1 +1,17 @@
-// Implement the built-in Pick<T, K> generic without using it.
+interface Todo {
+    title: string,
+    description: string,
+    completed: boolean
+}
+
+type test = {[key: string]: any}
+
+type MyPick<T extends object, k extends keyof T> = {
+    [key in k] : T[key]
+}
+type TodoPreview = MyPick<Todo, 'completed' | 'title' >
+
+const todo: TodoPreview = {
+    title : 'Clean room',
+    completed: false
+}
